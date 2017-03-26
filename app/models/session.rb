a905772b8b231998ab71cff88e65558232ab047b
Stephen Wisner <spwisner@gmail.com>
2017-03-26 12:16:37 -0400

    Created ranges for accepted session values
    Ranges permitted added to create session form

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,6 +4,8 @@ class Session < ApplicationRecord
   belongs_to :user
   belongs_to :student, inverse_of: :sessions
   has_many :observations, dependent: :destroy
+  validates :int_num, :inclusion => 1..60
+  validates :obs_time, :inclusion => 5..30
   validates :obs_setting, :obs_task,
             :obs_time, :int_num, presence: true
 end
