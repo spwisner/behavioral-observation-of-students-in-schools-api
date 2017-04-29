38710e908a5fed5a9574fe8246c0c785a3d1f65f
Stephen Wisner <spwisner@gmail.com>
2017-04-29 11:00:35 -0400

    Removed grade as being a required value

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,5 @@ class Student < ApplicationRecord
   belongs_to :user
   has_many :sessions, dependent: :destroy
   has_many :reports, dependent: :destroy
-  validates_presence_of :first_name, :last_name,
-                        :grade
+  validates_presence_of :first_name, :last_name
 end
